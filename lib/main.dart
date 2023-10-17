17b41ba0380bebdb787f186a34a3989292fc52d6
sangil <fhemflrh60@gmail.com>
2023-10-17 22:05:36 +0900

    랜덤 숫자 생성기

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:random_number_generator/screen/homeScreen.dart';
+//이미지: Freepik.com
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
